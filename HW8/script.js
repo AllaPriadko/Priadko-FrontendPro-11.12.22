af842feb458d94d2c3144fdd2537c6ab0d130f5b
Alla Priadko <105867410+AllaPriadko@users.noreply.github.com>
2022-12-18 20:48:12 +0100

    Update script.js

--- HW8/script.js
+++ HW8/script.js
@@ -1,12 +1,13 @@
 let number = 10;
 let result = '';
+
 console.group(`1. Вивести на сторінку в один рядок через кому числа від 10 до 20.`); 
     while (number <= 20) {
         result += (`${number},`);
         number++
     }
-     console.log(result);
-     console.groupEnd();
+console.log(result);
+console.groupEnd();
 
 
 console.group(`2. Вивести квадрати чисел від 10 до 20.`); 
@@ -16,7 +17,7 @@ console.group(`2. Вивести квадрати чисел від 10 до 20.`
         console.log(i * i);
         i++
     }
- console.groupEnd();
+console.groupEnd();
 
 
 console.group(`3. Вивести таблицю множення на 7.`); 
@@ -24,15 +25,15 @@ console.group(`3. Вивести таблицю множення на 7.`);
     for (let i = 1; i < 11; i++) { 
         console.log(`${i} * 7 = ${i * 7}`);
     }
- console.groupEnd();
+console.groupEnd();
 
 console.group(`4. Знайти добуток усіх цілих чисел від 15 до 35.`); 
     let mult = 1;
     for (let i = 15; i <= 35; i++){
         mult = mult * i;
     }
-     console.log(mult);
-     console.groupEnd();
+console.log(mult);
+console.groupEnd();
 
 
 console.group(`5. Знайти середнє арифметичне всіх цілих чисел від 1 до 500.`); 
@@ -40,8 +41,8 @@ console.group(`5. Знайти середнє арифметичне всіх ц
     for (let i = 1; i <= 500; i++){
         result1 = result1 + i;
     }
-    console.log(result1 / 500);
-    console.groupEnd();
+console.log(result1 / 500);
+console.groupEnd();
 
 console.group(`6. Вивести суму лише парних чисел в діапазоні від 30 до 80.`); 
     let sum = 0;
@@ -50,6 +51,6 @@ console.group(`6. Вивести суму лише парних чисел в д
          sum = sum + i;
         }
     }
-    console.log(sum);
-    console.groupEnd();
+console.log(sum);
+console.groupEnd();
 
