c88ca31e6131c6c1009feec0140d539d3dc82d5f
Alla Priadko <105867410+AllaPriadko@users.noreply.github.com>
2022-12-24 14:54:06 +0100

    Update script.js

--- HW11/script.js
+++ HW11/script.js
@@ -2,26 +2,25 @@ const firstArray = ['Apple', 'Banana', 'Orange', 'Kiwi', 'Granat']
 const secondArray = [ 'Pineapple', 'Peach','Kiwi', 'Pear', 'Banana',]
 
 function createArr (array1, array2) {
-    const empty = [];
+    const finalArr = [];
 
-    if (!(Array.isArray(array1) || Array.isArray(array2))) {
-        return empty;
+    if (!(Array.isArray(array1) && Array.isArray(array2))) {
+        console.log(`Error: not an Arrey`);
+        return finalArr;
     }
-    
-    const lastArr = [];
 
     for (let i = 0; i < array1.length; i++){
         const elementFirst = array1[i];
         const elementSecond = array2[i];
           
         if (!(array1.includes(elementSecond))) {
-            lastArr.push(elementSecond);
+            finalArr.push(elementSecond);
         }
         if (!(array2.includes(elementFirst))) {
-            lastArr.push(elementFirst)       
+            finalArr.push(elementFirst)       
         } 
     }
-    return lastArr;
+    return finalArr;
 }
  
 console.log(createArr(firstArray, secondArray));
