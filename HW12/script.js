bf5afb52cb93b7f9550cc3e956fb452565730491
Alla Priadko <105867410+AllaPriadko@users.noreply.github.com>
2022-12-27 11:39:45 +0100

    Update script.js

--- HW12/script.js
+++ HW12/script.js
@@ -2,12 +2,14 @@ const array = [1, 2, 3, 4, 5, 6, 7];
 
 function removeElement(array, item) {
 
-    let newArr;
-    if (array.splice(item - 1, 1)) {
-        newArr = array.slice();
-    }
+let newArr;
+    
+if (array.splice(item - 1, 1)) {
+    
+  newArr = array.slice();
+}
 
-    return newArr;
+  return newArr;
 }
         
 removeElement(array, 5);
